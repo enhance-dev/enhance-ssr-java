1f2d9a8754038215a52560c862c8a0f18ada7924
macdonst <simon.macdonald@gmail.com>
2024-02-26 16:21:40 -0500

    First commit
    Signed-off-by: macdonst <simon.macdonald@gmail.com>

--- README.md
+++ README.md
@@ -1,2 +1,40 @@
 # enhance-ssr-java
+
 Enhance SSR for Java
+
+## Prerequisites
+
+1. Install Java
+2. Install Gradle
+
+## Install Extism CLI
+
+```sh
+curl https://get.extism.org/cli | sh
+```
+
+## Install Extism Shared Library
+
+```sh
+sudo extism lib install latest
+```
+
+## Download Enhance SSR wasm
+
+Download the latest release of the compiled wasm:
+
+```sh
+curl -L https://github.com/enhance-dev/enhance-ssr-wasm/releases/download/v0.0.3/enhance-ssr.wasm.gz | gunzip > enhance-ssr.wasm
+```
+
+## Run the demo
+
+```sh
+./gradlew run
+```
+
+## Acknowledgements
+
+🎩 h/t to [Mario Hamann](https://github.com/mariohamann) for inspiring this repo form his work in his [java demo](https://github.com/mariohamann/enhance-ssr-wasm/tree/experiment/extism/java-demo) repo.
+
+
---
+++ src/main/java/EnhanceSsrWasm.java
@@ -0,0 +1,36 @@
+
+import org.extism.sdk.Plugin;
+import org.extism.sdk.manifest.Manifest;
+import org.extism.sdk.wasm.PathWasmSource;
+
+import java.util.List;
+import java.util.Map;
+
+public class EnhanceSsrWasm {
+    public static void main(String[] args) {
+        try {
+            // Hint: path starts from the root of the java project
+            String wasmPath = "./enhance-ssr.wasm";
+            String wasmName = "enhance-ssr";
+
+            Manifest manifest = new Manifest(List.of(new PathWasmSource(wasmName, wasmPath, null)));
+            Plugin plugin = new Plugin(manifest, true, null);
+
+            Map<String, Object> input = Map.of(
+                    "markup", "<my-header>Hello world!</my-header>",
+                    "elements", Map.of(
+                            "my-header", "function MyHeader({ html }) { return html`<h1><slot></slot></h1> ` }"
+                    ),
+                    "initialState", List.of()
+            );
+
+            String inputJson = new com.google.gson.Gson().toJson(input);
+
+            String output = plugin.call("ssr", inputJson);
+
+            System.out.println("Output: " + output);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
