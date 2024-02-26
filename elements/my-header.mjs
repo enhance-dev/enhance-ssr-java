438ab1f3142b1a794d23239085eeca68dcb51ea3
macdonst <simon.macdonald@gmail.com>
2024-02-26 17:07:39 -0500

    Read elements from folder
    Signed-off-by: macdonst <simon.macdonald@gmail.com>

---
+++ elements/my-header.mjs
@@ -0,0 +1,3 @@
+function MyHeader({ html }) {
+  return html`<style>h1 { color: red; }</style><h1><slot></slot></h1>`
+}
--- src/main/java/EnhanceSsrWasm.java
+++ src/main/java/EnhanceSsrWasm.java
@@ -3,10 +3,36 @@ import org.extism.sdk.Plugin;
 import org.extism.sdk.manifest.Manifest;
 import org.extism.sdk.wasm.PathWasmSource;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class EnhanceSsrWasm {
+
+    private static Map<String, String> readElements(String directory) {
+        File folder = new File(directory);
+        Map<String, String> elements = new HashMap<String, String>();
+
+        for (final File fileEntry : folder.listFiles()) {
+            if (!fileEntry.isDirectory()) {
+                try {
+                    String name = fileEntry.getName();
+                    Path fileName = Path.of(fileEntry.getAbsolutePath());
+                    String value = Files.readString(fileName);
+
+                    elements.put(name.substring(0, name.lastIndexOf(".")), value);
+                } catch (IOException ioe) {
+                    System.out.println("Failed to read file");
+                }
+            }
+        }
+
+        return elements;
+    }
     public static void main(String[] args) {
         try {
             // Hint: path starts from the root of the java project
@@ -18,9 +44,7 @@ public class EnhanceSsrWasm {
 
             Map<String, Object> input = Map.of(
                     "markup", "<my-header>Hello world!</my-header>",
-                    "elements", Map.of(
-                            "my-header", "function MyHeader({ html }) { return html`<h1><slot></slot></h1> ` }"
-                    ),
+                    "elements", readElements("elements"),
                     "initialState", List.of()
             );
 
